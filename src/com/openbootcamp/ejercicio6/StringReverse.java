cfe752860373ab77245b1510ffbb82a49c208656
fernalp <almanzafernando@hotmail.com>
2023-02-07 10:09:57 -0500

    Resolución de ejercicio número 6 de Java Básico de Open Bootcamp utilizando otro método y se deja comentado

--- src/com/openbootcamp/ejercicio6/StringReverse.java
+++ src/com/openbootcamp/ejercicio6/StringReverse.java
@@ -6,5 +6,11 @@ public class StringReverse {
         StringBuilder stringReverse = new StringBuilder(str);
         String textReverse = stringReverse.reverse().toString();
         System.out.println(textReverse);
+        /* String reverse = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        System.out.println(reverse);
+         */
     }
 }
